0ec649eb52b60595261e1d6f32855a198c357fe2
Dursky <ctzg12@gmail.com>
2022-11-24 18:49:50 +0100

    feature: Change sync into async of initialize db file

--- src/modules/fileSetup/index.ts
+++ src/modules/fileSetup/index.ts
@@ -9,31 +9,43 @@ export const databaseBlueprint: databaseStructure = {
 };
 
 export const initializeDatabaseFile = () => {
-	try {
-		console.log("-> Reading database file...");
-		fs.accessSync(process.env.FILE_NAME as string, fs.constants.F_OK);
-		const readedData = fs.readFileSync(process.env.FILE_NAME as string, "utf-8");
+	/* Check if file exist - if not just create a new one with blueprint as content */
+	fs.access(process.env.FILE_NAME as string, fs.constants.F_OK, (err) => {
+		if (err) {
+			fs.writeFile(
+				process.env.FILE_NAME as string,
+				JSON.stringify(databaseBlueprint),
+				(err) => err && console.error({err}),
+			);
+			console.log("-> Setup database file complete");
+		} else {
+			console.log("-> Setuping database file...");
+			fs.readFile(process.env.FILE_NAME as string, "utf-8", (err, readedData) => {
+				/* Initial check if file contain any string data */
+				let parsedData: databaseStructure =
+					readedData.length > 0 ? JSON.parse(readedData) : databaseBlueprint;
 
-		/* Initial check if file contain any string data */
-		let parsedData: databaseStructure =
-			readedData.length > 0 ? JSON.parse(readedData) : databaseBlueprint;
+				/* Validate parsed data - if not correct - just fix it*/
+				if (
+					!parsedData.id ||
+					!parsedData.databaseName ||
+					parsedData.id.length == 0 ||
+					parsedData.databaseName.length == 0
+				) {
+					/* File exist, but doesn't have property content */
+					if (!parsedData.id || parsedData.id.length == 0) parsedData.id = databaseBlueprint.id;
+					if (!parsedData.databaseName || parsedData.databaseName.length == 0)
+						parsedData.databaseName = databaseBlueprint.databaseName;
+				}
 
-		/* Validate parsed data - if not correct - just fix it*/
-		if (
-			!parsedData.id ||
-			!parsedData.databaseName ||
-			parsedData.id.length == 0 ||
-			parsedData.databaseName.length == 0
-		) {
-			/* File exist, but doesn't have property content */
-			if (!parsedData.id || parsedData.id.length == 0) parsedData.id = databaseBlueprint.id;
-			if (!parsedData.databaseName || parsedData.databaseName.length == 0)
-				parsedData.databaseName = databaseBlueprint.databaseName;
+				/* If everything is correct - just save any changes into file */
+				fs.writeFile(
+					process.env.FILE_NAME as string,
+					JSON.stringify(parsedData),
+					(err) => err && console.error(err),
+				);
+				console.log("-> Setup database file complete");
+			});
 		}
-
-		fs.writeFileSync(process.env.FILE_NAME as string, JSON.stringify(parsedData));
-		console.log("-> Read database file complete");
-	} catch (err) {
-		fs.writeFileSync(process.env.FILE_NAME as string, JSON.stringify(databaseBlueprint));
-	}
+	});
 };
