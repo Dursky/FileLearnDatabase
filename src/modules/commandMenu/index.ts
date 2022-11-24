9694673c0429371832581ffeb48c261478b9a798
WounderDragon <46166716+WounderDragon@users.noreply.github.com>
2022-11-24 12:11:14 +0100

    feature: added command line interface

--- src/modules/commandMenu/index.ts
+++ src/modules/commandMenu/index.ts
@@ -1,7 +1,7 @@
 
 //export type acceptableCommandsEnum = { 'showTables'|'createTables' ,'showTable'}
 
-export const acceptableCommands = ['showTables','createTables','showTable'];
+export const acceptableCommands = ['showTable','showTables','createTable'];
 
 export const commandMenu = () => {
     let argList: string[] = process.argv;
@@ -11,19 +11,36 @@ export const commandMenu = () => {
 
     if (argList[0] && argList[1]) {
         /* Check if key and value exist */
-        const key = argList[0].replace('-','').replace('-','');
-        if(acceptableCommands.includes(key)) {
-            /* Correct command from list*/
-            console.log({key})
+        const value = argList[1];
 
-            switch (key) {
+        const argumentsObj = argList.join(' ').split("--").filter((i) => i.length !== 0).map((i) => i.trim()).map((i) => ({[i.split(' ')[0]]: i.split(' ')[1]}) )
+
+        
+       argumentsObj.map((i) => Object.keys(i)[0]).map((command) => {
+       
+        if(acceptableCommands.includes(command)) {
+            /* Correct command from list*/
+            const commandValue =  Object.values(argumentsObj.filter((i) => Object.keys(i)[0] == command)[0])[0]
+
+            switch (command) {
+                case 'showTable':
+                    console.log('showTable',commandValue);
+                    break;
+
                 case 'showTables':
-                    
+                    console.log( 'showTables', commandValue)
+                    break;
+
+                case 'createTable' :
+                    console.log('createTable', commandValue)
+                    break;
             }
         }
+    })
+
        
     }
 
-    console.log({argList})
+
 
 }
